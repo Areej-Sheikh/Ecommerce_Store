62b5d772638b9800bbb6d7c4b408628f4e283c99
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-03-28 22:11:02 +0530

    updated cors

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 // app.use(cors());
-app.use(cors({ origin: process.env.FRONTEND_URL }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use("/images", express.static("uploads/images"));
 
 // Connect to MongoDB
