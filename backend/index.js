8bea30dddfe1058688aeff8d497d35765574b54e
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-03-28 21:40:04 +0530

    frontend linking to backend

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
-// app.use(cors({ origin: process.env.FRONTEND_URL }));
+// app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use("/images", express.static("uploads/images"));
 
 // Connect to MongoDB
